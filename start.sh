26af0170977f01f24f54478b076db386e524ff21
AngelSweety05 <moviesadda.pro@gmail.com>
2024-12-31 11:56:19 -0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,12 @@
-#!/bin/bash
-
-# Check if UPSTREAM_REPO is set and clone the appropriate repository
-if [ -z "$UPSTREAM_REPO" ]; then
-  echo "Cloning main Repository..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
   git clone https://github.com/lazyindu/test1.git /LazyPrincess
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO..."
-  git clone "$UPSTREAM_REPO" /LazyPrincess
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LazyPrincess
 fi
-
-# Navigate to the LazyPrincess directory
-cd /LazyPrincess || exit
-
-# Install Python dependencies
-if [ -f requirements.txt ]; then
-  echo "Installing dependencies from requirements.txt..."
-  pip3 install -U -r requirements.txt
-else
-  echo "No requirements.txt found. Skipping dependency installation."
-fi
-
-# Start the bot
-echo "Starting Bot..."
-exec python3 bot.py
+cd /LazyPrincess
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
